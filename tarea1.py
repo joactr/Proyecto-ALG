056a6d32e0aa445665d876997270601ff29eafd2
joactr <j.m.molol@hotmail.com>
2021-09-26 00:15:51 +0200

    first commit

---
+++ tarea1.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def dp_levenshtein_backwards(x, y):
+    tam_x = len(x) + 1
+    tam_y = len(y) + 1
+    tam_max = max(tam_x,tam_y)
+    list1 =  
+    list2 = 
+
+
+
+
+
+
+    return 0 # reemplazar/completar
+
+    def dp_restricted_damerau_backwards(x, y):
+    return 0 # reemplazar/completar
+
+def dp_intermediate_damerau_backwards(x, y):
+    return 0 # reemplazar/completar
+
+test = [("algoritmo","algortimo"),
+        ("algoritmo","algortximo"),
+        ("algoritmo","lagortimo"),
+        ("algoritmo","agaloritom"),
+        ("algoritmo","algormio"),
+        ("acb","ba")]
+
+for x,y in test:
+    print(f"{x:12} {y:12}",end="")
+    for dist,name in ((dp_levenshtein_backwards,"levenshtein"),
+                      (dp_restricted_damerau_backwards,"restricted"),
+                      (dp_intermediate_damerau_backwards,"intermediate")):
+        print(f" {name} {dist(x,y):2}",end="")
+    print()
+                 
+"""
+Salida del programa:
+algoritmo    algortimo    levenshtein  2 restricted  1 intermediate  1
+algoritmo    algortximo   levenshtein  3 restricted  3 intermediate  2
+algoritmo    lagortimo    levenshtein  4 restricted  2 intermediate  2
+algoritmo    agaloritom   levenshtein  5 restricted  4 intermediate  3
+algoritmo    algormio     levenshtein  3 restricted  3 intermediate  2
+acb          ba           levenshtein  3 restricted  3 intermediate  2
+"""         
